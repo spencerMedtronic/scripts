a43bee804bbb0ef57018c9fe14879079fecce4db
spencerMedtronic <spencer.p.drach@medtronic.com>
2020-11-10 15:44:50 -0600

    fgd

--- sshAll.py
+++ sshAll.py
@@ -1,6 +1,6 @@
 from pssh.clients import ParallelSSHClient
 
-client = ParallelSSHClient(['vagrant@haol3-d28', 'vagrant@phamt35-d16', 'vagrant@phamt35-d17'])
+client = ParallelSSHClient(['haol3-d28', 'phamt35-d16', 'phamt35-d17'])
 output = client.run_command('uname')
 for host_out in output:
     for line in host_out.stdout:
