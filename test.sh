9acac94e10548916c21db1c97a901e734c6272bb
spencerMedtronic <spencer.p.drach@medtronic.com>
2020-11-11 11:19:15 -0600

    small fix

--- test.sh
+++ test.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-while read line; do sshpass -p "fnJMM(JWm0TetkQ" ssh-copy-id -i ~/.ssh/id_rsa.pub vagrant@$server; done < TNAhosts.txt
+while read line; do
+  sshpass -p "fnJMM(JWm0TetkQ" ssh-copy-id -i ~/.ssh/id_rsa.pub vagrant@$line;
+done < TNAhosts.txt
