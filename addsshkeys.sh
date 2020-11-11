dd7bec5c33b967bd4525c881939550fc40bc543e
spencerMedtronic <spencer.p.drach@medtronic.com>
2020-11-11 14:21:06 -0600

    small change

--- addsshkeys.sh
+++ addsshkeys.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+#usage: ./addsshkeys.sh hostlist.txt
+
 while read line; do
   sshpass -p "fnJMM(JWm0TetkQ" ssh-copy-id -i ~/.ssh/id_rsa.pub vagrant@$line;
-done < TNAhosts.txt
+done < $1
