da88bcc90d9a4f00471e9a2cc0e324fcf92ecae3
spencerMedtronic <spencer.p.drach@medtronic.com>
2020-11-11 11:18:00 -0600

    should work

--- test.sh
+++ test.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-while read line; do echo $line; done < TNAhosts.txt
+while read line; do sshpass -p "fnJMM(JWm0TetkQ" ssh-copy-id -i ~/.ssh/id_rsa.pub vagrant@$server; done < TNAhosts.txt
