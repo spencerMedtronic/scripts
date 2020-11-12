b9d00d23006b029e4bda969724d69e8c3be2d804
drachs1 <spencer.p.drach@gmail.com>
2020-11-12 13:55:33 -0600

    ssh into all nodes and change apt mirror

--- kill-prune-docker.sh
+++ kill-prune-docker.sh
@@ -3,5 +3,5 @@
 #usage: ./kill-prine-docker.sh hostlist.txt
 PASS='fnJMM(JWm0TetkQ'
 while read -u10 line; do
-  ssh vagrant@$line "docker stop $(docker ps -a -q) && yes | docker system prune && echo '$PASS' | ./tnUp.sh";
+  ssh vagrant@$line "docker stop $(docker ps -aq) && yes | docker system prune && echo '$PASS' | ./tnUp.sh";
 done 10< $1
--- sshAll.sh
+++ sshAll.sh
@@ -3,5 +3,5 @@
 #usage: ./sshAll.sh hostlist.txt
 PASS='fnJMM(JWm0TetkQ'
 while read -u10 line; do
-  ssh vagrant@$line "dos2unix --version";
+  ssh vagrant@$line "echo $PASS | sudo sed -i 's/http:\/\/us.archive.ubuntu.com\/ubuntu/https:\/\/mirrors.bloomu.edu\/ubuntu/g' /etc/apt/sources.list; sudo apt-get update";
 done 10< $1
