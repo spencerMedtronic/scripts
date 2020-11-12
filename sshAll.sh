d5df0808e80cad573e39cb64017d6f4941ef3e94
drachs1 <spencer.p.drach@gmail.com>
2020-11-12 14:01:28 -0600

    this should work

--- sshAll.sh
+++ sshAll.sh
@@ -3,5 +3,5 @@
 #usage: ./sshAll.sh hostlist.txt
 PASS='fnJMM(JWm0TetkQ'
 while read -u10 line; do
-  ssh vagrant@$line "echo 'fnJMM(JWm0TetkQ' | sudo sed -i 's/http:\/\/us.archive.ubuntu.com\/ubuntu/https:\/\/mirrors.bloomu.edu\/ubuntu/g' /etc/apt/sources.list; sudo apt-get update";
+  ssh vagrant@$line "echo 'fnJMM(JWm0TetkQ' | sudo -S sed -i 's/http:\/\/us.archive.ubuntu.com\/ubuntu/https:\/\/mirrors.bloomu.edu\/ubuntu/g' /etc/apt/sources.list; sudo apt-get update";
 done 10< $1
