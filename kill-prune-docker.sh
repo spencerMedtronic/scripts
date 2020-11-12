728ab6cecee419c732f549bebeae0037f458cdb2
drachs1 <spencer.p.drach@gmail.com>
2020-11-12 14:27:20 -0600

    small fix

--- kill-prune-docker.sh
+++ kill-prune-docker.sh
@@ -3,5 +3,5 @@
 #usage: ./kill-prine-docker.sh hostlist.txt
 PASS='fnJMM(JWm0TetkQ'
 while read -u10 line; do
-  ssh vagrant@$line "docker stop $(docker ps -aq) && yes | docker system prune && echo '$PASS' | ./tnUp.sh";
+  ssh vagrant@$line "docker stop $(docker ps -aq) && yes | docker system prune && echo '$PASS' | sudo -S ./tnUp.sh";
 done 10< $1
