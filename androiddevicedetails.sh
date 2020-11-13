175412c94bb1cc617280421909daee5895021e90
spencerMedtronic <spencer.p.drach@medtronic.com>
2020-11-13 15:20:32 -0600

    if this fixes it I'll be upset

--- androiddevicedetails.sh
+++ androiddevicedetails.sh
@@ -3,7 +3,7 @@
 #usage: ./androidevicedetails.sh hostlist.txt
 while read -u10 line; do
   ssh vagrant@$line "hostname;\
-  for container in 'docker ps -f name = testnodeandroid4'; do\
+  for container in 'docker ps -f name=testnodeandroid4'; do\
   docker inspect --format='{{.Name}}' $container;\
   docker exec -it $container bash -c 'adb shell getprop ril.serialnumber; adb shell getprop ro.product.model';\
   done";
