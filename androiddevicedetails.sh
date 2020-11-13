67b347a6884f1c256439a9674dea59f34b003f05
spencerMedtronic <spencer.p.drach@medtronic.com>
2020-11-13 15:13:43 -0600

    ltes see if this works

--- androiddevicedetails.sh
+++ androiddevicedetails.sh
@@ -3,7 +3,7 @@
 #usage: ./androidevicedetails.sh hostlist.txt
 while read -u10 line; do
   ssh vagrant@$line "hostname;\
-  for container in 'docker ps -f name = testnodeandroid4'; \
-  do docker exec -it $container /bin/bash -c 'adb shell getprop ril.serialnumber; adb shell getprop ro.product.model';\
+  for container in 'docker ps -f name = testnodeandroid4'; do\
+  docker exec -it $container bash -c 'adb shell getprop ril.serialnumber; adb shell getprop ro.product.model';\
   done";
 done 10< $1
