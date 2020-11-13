524ea45cf42c1fc920ef2690c85ed1f272228c40
spencerMedtronic <spencer.p.drach@medtronic.com>
2020-11-13 15:15:05 -0600

    one more time

--- androiddevicedetails.sh
+++ androiddevicedetails.sh
@@ -4,6 +4,7 @@
 while read -u10 line; do
   ssh vagrant@$line "hostname;\
   for container in 'docker ps -f name = testnodeandroid4'; do\
+  docker inspect --format='{{.Name}}' $container;\
   docker exec -it $container bash -c 'adb shell getprop ril.serialnumber; adb shell getprop ro.product.model';\
   done";
 done 10< $1
