221acc930a91cc939bdc17e1215230ece9c257d3
spencerMedtronic <spencer.p.drach@medtronic.com>
2020-11-13 15:21:51 -0600

    that did fix it

--- androiddevicedetails.sh
+++ androiddevicedetails.sh
@@ -4,7 +4,8 @@
 while read -u10 line; do
   ssh vagrant@$line "hostname;\
   for container in 'docker ps -f name=testnodeandroid4'; do\
-  docker inspect --format='{{.Name}}' $container;\
-  docker exec -it $container bash -c 'adb shell getprop ril.serialnumber; adb shell getprop ro.product.model';\
+    echo $container;\
+    docker inspect --format='{{.Name}}' $container;\
+    docker exec -it $container bash -c 'adb shell getprop ril.serialnumber; adb shell getprop ro.product.model';\
   done";
 done 10< $1
